21c7ea959f181aae7f992990f00f6ff8faeee870
schtonn <schtonn@163.com>
2023-01-01 09:29:19 +0800

    whatever

--- blog/js/spoiler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-!function (e) {
-    [].forEach.call(e.getElementsByClassName('spoiler'), function (e) {
-        e.getElementsByClassName('spoiler-title')[0].onclick = function () {
-            e.classList.toggle('collapsed'), e.classList.toggle('expanded');
-        };
-    });
-}(document);
